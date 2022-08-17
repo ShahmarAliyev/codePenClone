fe788102c12283028aa236bd1f054b998784be06
ShahmarALiyev <saliyev2019@gmail.com>
2022-08-16 22:02:19 -0500

    Added esbuild and transformed the code input

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,56 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import * as esbuild from "esbuild-wasm";
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
+import { useEffect, useRef, useState } from "react";
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const App = () => {
+  const ref = useRef<any>();
+  const [input, setInput] = useState("");
+  const [code, setCode] = useState("");
+
+  const startService = async () => {
+    ref.current = await esbuild.startService({
+      worker: true,
+      wasmURL: "/esbuild.wasm",
+    });
+  };
+
+  useEffect(() => {
+    startService();
+  }, []);
+
+  const onClick = async () => {
+    if (!ref.current) {
+      return;
+    }
+    const result = await ref.current.transform(input, {
+      loader: "jsx",
+      target: "es2015",
+    });
+    setCode(result.code);
+  };
+
+  return (
+    <div>
+      <textarea value={input} onChange={(e) => setInput(e.target.value)}>
+        {input}
+      </textarea>
+      <div>
+        <button onClick={onClick} type="submit">
+          transpile
+        </button>
+      </div>
+      <pre>{code}</pre>
+    </div>
+  );
+};
+const el = document.getElementById("root");
+if (el === null) throw new Error("Root container missing in index.html");
+
+const root = ReactDOM.createRoot(el)!;
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
