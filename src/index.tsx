bfa824870fe4c89da001105e839ea96e552f1aec
ShahmarALiyev <saliyev2019@gmail.com>
2022-08-18 20:35:30 -0500

    Adding plugin to help download the packages from unpkg instead of filesystem

--- src/index.tsx
+++ src/index.tsx
@@ -1,21 +1,19 @@
-import * as esbuild from "esbuild-wasm";
-
-import { useEffect, useRef, useState } from "react";
-import React from "react";
-import ReactDOM from "react-dom/client";
+import * as esbuild from 'esbuild-wasm';
+import { useState, useEffect, useRef } from 'react';
+import ReactDOM from 'react-dom';
+import { unpkgPathPlugin } from './plugins/unpkg-path-plugin';
 
 const App = () => {
   const ref = useRef<any>();
-  const [input, setInput] = useState("");
-  const [code, setCode] = useState("");
+  const [input, setInput] = useState('');
+  const [code, setCode] = useState('');
 
   const startService = async () => {
     ref.current = await esbuild.startService({
       worker: true,
-      wasmURL: "/esbuild.wasm",
+      wasmURL: '/esbuild.wasm',
     });
   };
-
   useEffect(() => {
     startService();
   }, []);
@@ -24,33 +22,35 @@ const App = () => {
     if (!ref.current) {
       return;
     }
-    const result = await ref.current.transform(input, {
-      loader: "jsx",
-      target: "es2015",
+
+    const result = await ref.current.build({
+      entryPoints: ['index.js'],
+      bundle: true,
+      write: false,
+      plugins: [unpkgPathPlugin()],
+      define: {
+        'process.env.NODE_ENV': '"production"',
+        global: 'window',
+      },
     });
-    setCode(result.code);
+
+    // console.log(result);
+
+    setCode(result.outputFiles[0].text);
   };
 
   return (
     <div>
-      <textarea value={input} onChange={(e) => setInput(e.target.value)}>
-        {input}
-      </textarea>
+      <textarea
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+      ></textarea>
       <div>
-        <button onClick={onClick} type="submit">
-          transpile
-        </button>
+        <button onClick={onClick}>Submit</button>
       </div>
       <pre>{code}</pre>
     </div>
   );
 };
-const el = document.getElementById("root");
-if (el === null) throw new Error("Root container missing in index.html");
 
-const root = ReactDOM.createRoot(el)!;
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+ReactDOM.render(<App />, document.querySelector('#root'));
