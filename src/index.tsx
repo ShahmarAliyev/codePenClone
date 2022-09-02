546962a132d9e6e1dd3581915630e8f021557fda
ShahmarALiyev <saliyev2019@gmail.com>
2022-09-01 19:24:31 -0500

    separated caching logic with extra onload function

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,7 @@ import { useState, useEffect, useRef } from "react";
 import ReactDOM from "react-dom";
 import { unpkgPathPlugin } from "./plugins/unpkg-path-plugin";
 import { fetchPlugin } from "./plugins/fetch-plugin";
+
 const App = () => {
   const ref = useRef<any>();
   const [input, setInput] = useState("");
@@ -11,7 +12,7 @@ const App = () => {
   const startService = async () => {
     ref.current = await esbuild.startService({
       worker: true,
-      wasmURL: "/esbuild.wasm",
+      wasmURL: "https://unpkg.com/esbuild-wasm@0.8.27/esbuild.wasm",
     });
   };
   useEffect(() => {
